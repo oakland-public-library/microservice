ddcc8c820512d50199b9b6eabbf7a579a9021d10
Windsor Schmidt <windsor.schmidt@gmail.com>
2018-04-23 15:46:25 -0700

    Display more report parameters in high-demand holds template

--- ils_report.py
+++ ils_report.py
@@ -33,16 +33,16 @@ SELECT
     SELECT order_record_id
     FROM sierra_view.bib_record_order_record_link bol
     WHERE bol.bib_record_id = h.record_id
-  ) AS totorders /* Return list of order records for a given bib with hold, 
+  ) AS totorders /* Return list of order records for a given bib with hold,
   then sum up the eligible copies */
   JOIN sierra_view.order_record orrec
   ON orrec.record_id = totorders.order_record_id
   JOIN sierra_view.order_record_cmf orf
   ON orf.order_record_id = orrec.record_id
   where
-  /* Return only orders records that are not received, and exclude multi 
+  /* Return only orders records that are not received, and exclude multi
   copies, otherwise counts will be duplicated */
-  orrec.received_date_gmt IS NULL and orrec.order_status_code = 'o' 
+  orrec.received_date_gmt IS NULL and orrec.order_status_code = 'o'
   and orf.location_code != 'multi')
   AS order_copies_count
 FROM
@@ -53,17 +53,19 @@ WHERE
     h.status = '0' /* Look for hold records with on-hold status only */
 GROUP BY h.record_id, brp.best_title, holdcount
 ORDER BY 3 DESC
-) AS derivedtable /*Create derived table from subquery before 
+) AS derivedtable /*Create derived table from subquery before
 filtering out based on ratio*/
 /* Set the hold to item threshold here */
-WHERE (holdcount::DECIMAL / COALESCE(NULLIF(itemcount,0),1))::DECIMAL >= (%s)"""
+WHERE (holdcount::DECIMAL / COALESCE(NULLIF(itemcount,0),1))::DECIMAL >= (%s)
+"""
     cur = conn.cursor()
     cur.execute(sql, (ratio,))
     results = cur.fetchall()
     cur.close()
     results = [list(elem) for elem in results]
     for x in results:
-        x.append('{0:.2f}'.format(int(x[2]) if x[3] == 0 else int(x[2])/int(x[3])))
+        x.append('{0:.2f}'.format(int(x[2]) if x[3] == 0 else
+                                  int(x[2]) / int(x[3])))
     header = [{'label': 'Bib Record'},
               {'label': 'Bib Title'},
               {'label': 'Holds'},
@@ -73,7 +75,7 @@ WHERE (holdcount::DECIMAL / COALESCE(NULLIF(itemcount,0),1))::DECIMAL >= (%s)"""
     report = {'header': header,
               'data': results,
               'description': 'High Demand Holds',
-              'date': datetime.datetime.now().isoformat(),
+              'date': datetime.datetime.now(),
               'ratio': ratio
               }
     return report
